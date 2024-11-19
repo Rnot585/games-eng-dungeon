7dae3103fe7ea8ce9c7d1a3799e8d4f671b3ba16
Rnot585 <40530741@live.napier.ac.uk>
2024-11-19 15:54:39 +0000

    branch push test

--- dungeon_dashi/main.cpp
+++ dungeon_dashi/main.cpp
@@ -1,7 +1,7 @@
 #include <SFML/Graphics.hpp>
 
 int main(){
-	sf::RenderWindow window(sf::VideoMode({200, 200}), "SFML works!");
+	sf::RenderWindow window(sf::VideoMode({200, 200}), "Dev branch test!");
 	sf::CircleShape shape(100.f);
 	shape.setFillColor(sf::Color::Green);
 	
